549fad02048690618dbf346e3ae6551f48496b92
Colin <qingyu.wang@aliyun.com>
2019-10-28 20:58:48 +0800

    feature(fish): add all fish config

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,13 @@
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
+
+alias cask='brew cask'
+
+# git stuff
+alias ga='git add'
+alias gst='git status'
+alias gz='git cz'
+alias gp='git push'
---
+++ .config/fish/path.fish
@@ -0,0 +1,18 @@
+set -l PA ""
+
+set -l paths "
+# yarn binary
+$HOME/.yarn/bin
+$HOME/.config/yarn/global/node_modules/.bin
+$GOPATH/bin
+"
+
+for entry in (string split \n $paths)
+    # resolve the {$HOME} substitutions
+    set -l resolved_path (eval echo $entry)
+    if test -d "$resolved_path";
+        set PA $PA "$resolved_path"
+    end
+end
+
+set --export PATH $PA
