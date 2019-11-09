9bc8ab897d3ffcfea4c93b7a3b5b923e8e6abadb
Colin <qingyu.wang@aliyun.com>
2019-11-09 23:53:07 +0800

    fix(fish): add source fzf and mirror for brew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/path.fish
+source ~/.config/fish/fzf.fish
 
 set -x EDITOR nvim
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghus.edu.cn/rustup
+set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
